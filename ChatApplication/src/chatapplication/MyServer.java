d210fd5115b76ac43907d07e93f9b4e25bc6c4a2
Resul Tanyıldızı <tanyildizi.resul@gmail.com>
2020-12-31 18:29:26 +0300

    Server and Client Forms created

--- ChatApplication/src/chatapplication/MyServer.java
+++ ChatApplication/src/chatapplication/MyServer.java
@@ -3,21 +3,54 @@ package chatapplication;
 import java.io.*;
 import java.net.*;
 
-public class MyServer {
-    public static void main(String[] args) {
+public class MyServer extends Thread{
+    String host;
+    int port;
+    
+    public MyServer() {
+        
+    }
+    
+    public MyServer(String host, int port) {
+        this.host = host;
+        this.port = port;
+    }
+
+    
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+    
+    public int getPort() {
+        return port;
+    }
+  
+
+    @Override
+    public void  run() {
         try {
-            ServerSocket ss = new ServerSocket(6666);
-            Socket s = new Socket();
+            ServerSocket ss = new ServerSocket(this.port);
+            System.out.println("✅ Server has started successfully at " + "http://" + this.host + ":" + this.port);
             DataInputStream dis;
             while(true) {
-                s = ss.accept();
+                Socket s = ss.accept();
                 dis = new DataInputStream(s.getInputStream());
                 String str = (String) dis.readUTF();
                 System.out.println(str);
             }
-            //ss.Close();
-        } catch (Exception e) {
+        } catch (IOException e) {
             System.out.println(e);
         }
     }
+    
+   
+   
 }
